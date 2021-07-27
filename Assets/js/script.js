7544647ccc4cb13658a08b1eca03192761acec51
cristobal hernandez chavez <cristobal.hernandez@ilumps.com>
2021-07-27 17:36:17 -0600

    Remade the html. Erased the JS almost completelly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-//Show current date
+//Show current date THIS IS FIRST JUST TO GET A CLEANER CODE AND SEPARATE PARTS
 var today = moment()
-
 $('#currentDay').text(today.format("dddd, MMM Do YYYY"))
 
+// Schedule code 
