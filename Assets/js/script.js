f6d24eb0e116e913505312af6e74bea46bf0b06b
cristobal hernandez chavez <cristobal.hernandez@ilumps.com>
2021-07-28 12:20:53 -0600

    Add formula to loop through time ids

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,23 @@
-//Show current date THIS IS FIRST JUST TO GET A CLEANER CODE AND SEPARATE PARTS
+//Show current date
 var today = moment()
 $('#currentDay').text(today.format("dddd, MMM Do YYYY"))
 
 // Schedule code 
+var dt = new Date()
+var hh = dt.getHours()
+
+
+console.log("Current hour: " + hh)
+
+
+//Color change each hour
+var timeList = ""
+$('.time-block').each(function() {
+    timeList = parseInt($('.time-block').attr('id').split("time")[1]); 
+    //parseInt transforms the items into an integer value. parse is for objects, parseInt is for integers
+    //this = the .time-block class
+    //attr = gets the attribute "id" from every first div
+    // split = Divides the string into 2 parts. Time and # and because of parseInt then we only get the number
+    
+})
+
